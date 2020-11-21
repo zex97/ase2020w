43fdc52877f7ffd34e6ea68d7ceb034fa3593a75
anastasia <e01448898@student.tuwien.ac.at>
2020-11-21 22:11:17 +0100

    update project structure

--- backend/rest-api/src/main/java/com/studyboard/config/StudyboardApplicationConfig.java
+++ backend/rest-api/src/main/java/com/studyboard/config/StudyboardApplicationConfig.java
@@ -1,8 +1,10 @@
-package com.studyboard.application;
+package com.studyboard.config;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan
 public class StudyboardApplicationConfig {
 
 }
