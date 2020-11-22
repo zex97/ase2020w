fac9dad9aaa5510f705052cae2a5dd7e7737ab1f
anastasia <e01448898@student.tuwien.ac.at>
2020-11-22 19:40:11 +0100

    prepare user space interface and exceptions

--- backend/user-space/src/main/java/com/studyboard/space/exception/SpaceDoesNotExist.java
+++ backend/user-space/src/main/java/com/studyboard/space/exception/SpaceDoesNotExist.java
@@ -1,4 +1,8 @@
 package com.studyboard.space.exception;
 
-public class SpaceNotFound {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Space not found.")
+public class SpaceDoesNotExist extends RuntimeException {
 }
