caaac255d48d880c8f48cebd9de63be138706b02
anastasia <e01448898@student.tuwien.ac.at>
2020-11-22 19:41:07 +0100

    partly expose user space service via rest api

--- backend/rest-api/src/main/java/com/studyboard/rest/UserSpaceController.java
+++ backend/rest-api/src/main/java/com/studyboard/rest/UserSpaceController.java
@@ -1,22 +1,58 @@
 package com.studyboard.rest;
 
 import com.studyboard.model.Space;
+import com.studyboard.model.User;
+import com.studyboard.repository.UserRepository;
 import com.studyboard.space.service.UserSpaceService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping(value="/api/space")
-public class SpaceController {
+public class UserSpaceController {
     @Autowired
     private UserSpaceService service;
 
-    @RequestMapping(value="/getAll", method = RequestMethod.GET, produces = "application/json")
-    public List<Space> getAllSpaces(){
-        return service.getAllSpaces();
+    @Autowired
+    private UserRepository repository;
+
+    @RequestMapping(value="/{userId}", method = RequestMethod.GET, produces = "application/json")
+    public List<Space> getUserSpaces(@PathVariable(name = "userId") long userId){
+        return service.getUserSpaces(userId);
+    }
+
+    @RequestMapping(value="/{userId}", method = RequestMethod.POST, produces = "application/json")
+    public ResponseEntity addUserSpace(@PathVariable(name = "userId") long userId, @RequestBody Space space){
+        service.addSpaceToUser(userId, space);
+        return ResponseEntity.ok().build();
+    }
+
+    @RequestMapping(value="/{userId}/{spaceId}", method = RequestMethod.DELETE, produces = "application/json")
+    public ResponseEntity deleteUserSpace(@PathVariable(name = "userId") long userId, @PathVariable(name = "spaceId") long spaceId){
+        service.removeSpaceFromUser(userId, spaceId);
+        return ResponseEntity.ok().build();
+    }
+
+    @RequestMapping(value="/users", method = RequestMethod.GET, produces = "application/json")
+    public ResponseEntity getUsers(){
+        return ResponseEntity.ok().body(repository.findAll());
+    }
+
+
+
+
+    @RequestMapping(value="/test", method = RequestMethod.GET, produces = "application/json")
+    public List<Space> runTest(){
+        User user = repository.save(new User());
+        Space spase = new Space();
+        spase.setName("ololo");
+        service.addSpaceToUser(user.getId(), spase);
+
+        List<Space> spaces = service.getUserSpaces(user.getId());
+
+        return spaces;
     }
 }
