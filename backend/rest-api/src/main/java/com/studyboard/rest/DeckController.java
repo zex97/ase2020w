d4ab12fd5a1bbfa3ce9f5c96edc0c5cfe19332c2
Tina Stamencic <tina.stamencic@tuwien.ac.at>
2020-11-23 14:30:42 +0100

    Some of the service methods via rest api

---
+++ backend/rest-api/src/main/java/com/studyboard/rest/DeckController.java
@@ -0,0 +1,34 @@
+package com.studyboard.rest;
+
+import com.studyboard.flashcard.service.DeckSpace;
+import com.studyboard.model.Deck;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value="/api/decks")
+public class DeckController {
+
+    @Autowired
+    private DeckSpace service;
+
+    @RequestMapping(value = "/", method = RequestMethod.GET, produces = "application/json")
+    public List<Deck> getAllDecks(){
+        return service.getAllDecks();
+    }
+
+    @RequestMapping(value = "/{deckId}", method = RequestMethod.GET, produces = "application/json")
+    public Deck getOneDeck(@PathVariable(name = "deckId") long deckId){
+        return service.getOneDeck(deckId);
+    }
+
+    @RequestMapping(value = "/", method = RequestMethod.POST, produces = "application/json")
+    public ResponseEntity createDeck(@RequestBody Deck deck) {
+        service.createDeck(deck);
+        return ResponseEntity.ok().build();
+    }
+
+ }
