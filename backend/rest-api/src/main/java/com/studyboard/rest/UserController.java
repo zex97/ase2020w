eb14d4ff9dd90fae8a5d0a64755e0691cebe789e
Zeljko Lukic <e1629048@student.tuwien.ac.at>
2020-11-25 22:25:20 +0100

    Added DTO class for User and adapted UserController to it.

--- backend/rest-api/src/main/java/com/studyboard/rest/UserController.java
+++ backend/rest-api/src/main/java/com/studyboard/rest/UserController.java
@@ -1,14 +1,16 @@
 package com.studyboard.rest;
 
+import com.studyboard.dto.UserDTO;
 import com.studyboard.exception.UniqueConstraintException;
-import com.studyboard.model.User;
 import com.studyboard.space.security.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
+@CrossOrigin
 @RestController
 @RequestMapping(value = "/api/user")
 public class UserController {
@@ -17,24 +19,24 @@ public class UserController {
     private UserService userService;
 
     @RequestMapping(value = "/{userId}", method = RequestMethod.GET, produces = "application/json")
-    public User getUser(@PathVariable(name = "userId") long userId) {
-        return userService.getUser(userId);
+    public UserDTO getUser(@PathVariable(name = "userId") long userId) {
+        return UserDTO.of(userService.getUser(userId));
     }
 
     @RequestMapping(method = RequestMethod.GET, produces = "application/json")
-    public List<User> getAllUsers() {
-        return userService.getAllUsers();
+    public List<UserDTO> getAllUsers() {
+        return userService.getAllUsers().stream().map(UserDTO::of).collect(Collectors.toList());
     }
 
     @RequestMapping(method = RequestMethod.POST, produces = "application/json")
-    public ResponseEntity createUser(@RequestBody User user) throws UniqueConstraintException {
-        userService.createUser(user);
+    public ResponseEntity createUser(@RequestBody UserDTO userDTO) throws UniqueConstraintException {
+        userService.createUser(userDTO.toUser());
         return ResponseEntity.ok().build();
     }
 
     @RequestMapping(method = RequestMethod.PUT, produces = "application/json")
-    public ResponseEntity editUserPassword(@RequestBody User user) {
-        userService.updateUserPassword(user);
+    public ResponseEntity editUserPassword(@RequestBody UserDTO userDTO) {
+        userService.updateUserPassword(userDTO.toUser());
         return ResponseEntity.ok().build();
     }
 
