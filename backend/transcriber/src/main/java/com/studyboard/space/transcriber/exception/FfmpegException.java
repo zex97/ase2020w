5d3034912ef45e729b162a5dfab9c4267a3c5676
Anastasiia Kabanova <e01448898@student.tuwien.ac.at>
2020-12-14 22:31:25 +0100

    #23 implemented ffmpeg interface

---
+++ backend/transcriber/src/main/java/com/studyboard/space/transcriber/exception/FfmpegException.java
@@ -0,0 +1,7 @@
+package com.studyboard.space.transcriber.exception;
+
+public class FfmpegException extends TranscriberException {
+    public FfmpegException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
---
+++ backend/transcriber/src/main/java/com/studyboard/space/transcriber/exception/FfmpegIllegalFormatException.java
@@ -0,0 +1,7 @@
+package com.studyboard.space.transcriber.exception;
+
+public class FfmpegIllegalFormatException extends TranscriberException{
+    public FfmpegIllegalFormatException(String message) {
+        super(message);
+    }
+}
