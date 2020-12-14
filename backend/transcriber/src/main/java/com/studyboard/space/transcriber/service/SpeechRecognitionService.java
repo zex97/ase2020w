858de3de8aa705fa1c31213c0dc272e6f579c1da
Anastasiia Kabanova <e01448898@student.tuwien.ac.at>
2020-12-14 22:33:03 +0100

    #23 added communication with the google api

---
+++ backend/transcriber/src/main/java/com/studyboard/space/transcriber/service/SpeechRecognitionService.java
@@ -0,0 +1,80 @@
+package com.studyboard.space.transcriber.service;
+
+import com.google.cloud.speech.v1.*;
+import com.google.protobuf.ByteString;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+@Service
+public class SpeechRecognitionService {
+    private final Logger logger = LoggerFactory.getLogger(SpeechRecognitionService.class);
+
+    private final RecognitionConfig config =
+            RecognitionConfig.newBuilder()
+                    .setEncoding(RecognitionConfig.AudioEncoding.LINEAR16)
+                    .setLanguageCode("en-US")
+                    .build();
+
+    /**
+     * This method is used to retrieve the transcription from Google Speech-to-Text API.
+     * The result is one combined transcription for all audio and video files in the directory.
+     * @param directoryPath absolute path to the source directory
+     * @return string with transcription
+     */
+    public String transcribeFilesInDirectory(String directoryPath){
+        File dirWithChunks = new File(directoryPath);
+        String[] fileNames = dirWithChunks.list();
+        StringBuilder transcriptionBuilder = new StringBuilder();
+        if (fileNames == null){
+            return transcriptionBuilder.toString();
+        }
+        for (String fileName : fileNames) {
+            transcriptionBuilder.append(transcribeFile(directoryPath + fileName));
+        }
+        return transcriptionBuilder.toString();
+    }
+
+    /**
+     * This method is used to retrieve the transcription from Google Speech-to-Text API.
+     * The result is a transcription for the given audio or video file.
+     * @param filePath absolute path to the source file
+     * @return string with transcription
+     */
+    public String transcribeFile(String filePath){
+        String transcription = "";
+        try (SpeechClient speech = SpeechClient.create()) {
+            RecognitionAudio requestBody = RecognitionAudio.newBuilder().setContent(getBytes(filePath)).build();
+            RecognizeResponse response = speech.recognize(config, requestBody);
+            transcription = extractResult(response.getResultsList());
+        } catch (IOException e){
+            logger.error("Could not get transcription for the file {}", filePath, e);
+        }
+        return transcription;
+    }
+
+    private String extractResult(List<SpeechRecognitionResult> results){
+        if (results.size() != 1) {
+            logger.warn("Size of the result list of chunks is not one");
+        }
+        StringBuilder transcript = new StringBuilder();
+        for (SpeechRecognitionResult result : results) {
+            transcript.append(result.getAlternativesList().get(0).getTranscript());
+        }
+        return transcript.toString();
+    }
+
+    private ByteString getBytes(String filePath) throws IOException {
+        Path path = Paths.get(filePath);
+        byte[] content = Files.readAllBytes(path);
+        ByteString bytes = ByteString.copyFrom(content);
+        return bytes;
+    }
+}
