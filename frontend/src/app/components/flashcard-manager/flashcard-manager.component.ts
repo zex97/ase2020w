fba8aa8c2ed2376b9f80e788606ebea5f93cd09d
Aleksa Glamocanin <e11722602@student.tuwien.ac.at>
2020-12-26 23:54:50 +0100

    [Issue #61] - Fix on rating of flashcards in backend

--- frontend/src/app/components/flashcard-manager/flashcard-manager.component.ts
+++ frontend/src/app/components/flashcard-manager/flashcard-manager.component.ts
@@ -314,7 +314,8 @@ export class FlashcardManagerComponent implements OnInit {
     if (rate != null) {
       flashcard.confidenceLevel = rate;
     }
-    if (this.currentRate < 1 || this.currentRate > 5){      this.error = true;
+    if (this.currentRate < 1 || this.currentRate > 5) {
+      this.error = true;
       this.errorMessage = 'Could not rate the flashcard! Please choose the value between 1 and 5.';
       this.openSnackbar(this.errorMessage, 'warning-snackbar');
     } else {
