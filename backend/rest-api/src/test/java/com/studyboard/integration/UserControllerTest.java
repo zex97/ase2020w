eb4d4541734eb220b1ed80380f4c1fa0981d5baf
Zeljko Lukic <e1629048@student.tuwien.ac.at>
2021-01-26 11:20:05 +0100

    [Issue #128] Added more user tests.

--- backend/rest-api/src/test/java/com/studyboard/integration/UserControllerTest.java
+++ backend/rest-api/src/test/java/com/studyboard/integration/UserControllerTest.java
@@ -1,5 +1,6 @@
 package com.studyboard.integration;
 
+import com.icegreen.greenmail.store.FolderException;
 import com.icegreen.greenmail.util.GreenMail;
 import com.icegreen.greenmail.util.GreenMailUtil;
 import com.icegreen.greenmail.util.ServerSetupTest;
@@ -9,6 +10,8 @@ import com.studyboard.model.PasswordResetToken;
 import com.studyboard.model.User;
 import com.studyboard.repository.ResetTokenRepository;
 import org.junit.jupiter.api.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
@@ -19,7 +22,9 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import javax.mail.Message;
+import java.util.Calendar;
 import java.util.List;
+import java.util.UUID;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -31,6 +36,8 @@ public class UserControllerTest extends BaseIntegrationTest {
     private static final String USER_ID_PATH = "/{userID}";
     private static final String USER_USERNAME_PATH = "/username{username}";
     private static final String USER_VERIFY_EMAIL_PATH = "/reset/{email}";
+    private static final String USER_VERIFY_TOKEN_PATH = "/reset/token/{token}";
+    private static final String USER_CHANGE_PASSW_TOKEN_PATH = "/reset/change/{token}";
 
     private static final User TEST_USER_1 = new User("testUsername1", "testPassword1", "user1@email.com", 2, "USER", true);
     private static final User TEST_USER_2 = new User("testUsername2", "testPassword2", "user2@email.com", 0, "USER", true);
@@ -46,6 +53,8 @@ public class UserControllerTest extends BaseIntegrationTest {
     private ResetTokenRepository resetTokenRepository;
 
     private GreenMail testSmtp;
+
+    private final Logger logger = LoggerFactory.getLogger(UserControllerTest.class);
 
     @BeforeAll
     public void testSmtpInit(){
@@ -65,6 +74,11 @@ public class UserControllerTest extends BaseIntegrationTest {
 
     @AfterEach
     void tearDown() {
+        try {
+            testSmtp.purgeEmailFromAllMailboxes();
+        } catch (FolderException e) {
+            logger.error("Error deleting test emails: " + e);
+        }
         JdbcTestUtils.deleteFromTables(jdbcTemplate, "password_reset_token", "sb_user", "user_roles");
     }
 
@@ -271,5 +285,179 @@ public class UserControllerTest extends BaseIntegrationTest {
         String link = "http://localhost:4200/changePassword?token=" + token.get(0).getToken();
         String expectedBody = "Hello " + user.getUsername() + "! Click on the link to reset you password. " + link;
         Assertions.assertEquals(expectedBody, body);
+    }
+
+    @Test
+    public void verifyResetToken() throws Exception {
+        User user = new User(TEST_USER_1);
+        String requestJson = convertObjectToStringForJson(user);
+
+        this.mockMvc
+                .perform(MockMvcRequestBuilders.post(USER_ENDPOINT).contentType(MediaType.APPLICATION_JSON).content(requestJson))
+                .andExpect(status().isOk());
+
+        ResultActions resultActionsUser =
+                this.mockMvc
+                        .perform(MockMvcRequestBuilders.get(USER_ENDPOINT).accept(MediaType.APPLICATION_JSON))
+                        .andExpect(status().isOk())
+                        .andExpect(MockMvcResultMatchers.jsonPath("$.length()").value(1));
+
+        String responseString = resultActionsUser.andReturn().getResponse().getContentAsString();
+        User[] responseArray = mapper.readValue(responseString, User[].class);
+
+        this.mockMvc
+                .perform(MockMvcRequestBuilders.post(USER_ENDPOINT + USER_VERIFY_EMAIL_PATH, TEST_USER_1.getEmail()))
+                .andExpect(status().isOk());
+
+        List<PasswordResetToken> token = resetTokenRepository.findAllByUserId(responseArray[0].getId());
+        Assertions.assertEquals(1, token.size());
+
+        this.mockMvc
+                .perform(MockMvcRequestBuilders.post(USER_ENDPOINT + USER_VERIFY_TOKEN_PATH, token.get(0).getToken()))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.token").value(token.get(0).getToken()));
+
+
+    }
+
+    @Test
+    public void verifyExpiredToken() throws Exception {
+        User user = new User(TEST_USER_1);
+        String requestJson = convertObjectToStringForJson(user);
+
+        this.mockMvc
+                .perform(MockMvcRequestBuilders.post(USER_ENDPOINT).contentType(MediaType.APPLICATION_JSON).content(requestJson))
+                .andExpect(status().isOk());
+
+        ResultActions resultActionsUser =
+                this.mockMvc
+                        .perform(MockMvcRequestBuilders.get(USER_ENDPOINT).accept(MediaType.APPLICATION_JSON))
+                        .andExpect(status().isOk())
+                        .andExpect(MockMvcResultMatchers.jsonPath("$.length()").value(1));
+
+        String responseString = resultActionsUser.andReturn().getResponse().getContentAsString();
+        User[] responseArray = mapper.readValue(responseString, User[].class);
+
+        this.mockMvc
+                .perform(MockMvcRequestBuilders.post(USER_ENDPOINT + USER_VERIFY_EMAIL_PATH, TEST_USER_1.getEmail()))
+                .andExpect(status().isOk());
+
+        List<PasswordResetToken> token = resetTokenRepository.findAllByUserId(responseArray[0].getId());
+        Assertions.assertEquals(1, token.size());
+
+        // 25 hours later
+        Calendar expires = Calendar.getInstance();
+        expires.add(Calendar.HOUR, -25);
+        token.get(0).setExpires(expires.getTimeInMillis());
+        resetTokenRepository.save(token.get(0));
+
+        this.mockMvc
+                .perform(MockMvcRequestBuilders.post(USER_ENDPOINT + USER_VERIFY_TOKEN_PATH, token.get(0).getToken()))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.token").value("invalid"));
+
+
+    }
+
+    @Test
+    public void changePasswordWithTokenSuccessfully() throws Exception {
+        User user = new User(TEST_USER_1);
+        String requestJson = convertObjectToStringForJson(user);
+
+        this.mockMvc
+                .perform(MockMvcRequestBuilders.post(USER_ENDPOINT).contentType(MediaType.APPLICATION_JSON).content(requestJson))
+                .andExpect(status().isOk());
+
+        ResultActions resultActionsUser =
+                this.mockMvc
+                        .perform(MockMvcRequestBuilders.get(USER_ENDPOINT).accept(MediaType.APPLICATION_JSON))
+                        .andExpect(status().isOk())
+                        .andExpect(MockMvcResultMatchers.jsonPath("$.length()").value(1));
+
+        String responseString = resultActionsUser.andReturn().getResponse().getContentAsString();
+        User[] responseArray = mapper.readValue(responseString, User[].class);
+
+        this.mockMvc
+                .perform(MockMvcRequestBuilders.post(USER_ENDPOINT + USER_VERIFY_EMAIL_PATH, TEST_USER_1.getEmail()))
+                .andExpect(status().isOk());
+
+        User userWithNewPassword = new User(TEST_USER_1);
+        userWithNewPassword.setPassword(TEST_UPDATED_PASSWORD);
+        String requestJson2 = convertObjectToStringForJson(userWithNewPassword);
+
+        List<PasswordResetToken> token = resetTokenRepository.findAllByUserId(responseArray[0].getId());
+        Assertions.assertEquals(1, token.size());
+
+        this.mockMvc
+                .perform(MockMvcRequestBuilders.post(USER_ENDPOINT + USER_CHANGE_PASSW_TOKEN_PATH, token.get(0).getToken())
+                .contentType(MediaType.APPLICATION_JSON).content(requestJson2))
+                .andExpect(status().isOk());
+
+        //token consumed
+        token = resetTokenRepository.findAllByUserId(responseArray[0].getId());
+        Assertions.assertEquals(0, token.size());
+
+        ResultActions resultActionsAfterPasswordChange =
+                this.mockMvc
+                        .perform(MockMvcRequestBuilders.get(USER_ENDPOINT).accept(MediaType.APPLICATION_JSON))
+                        .andExpect(status().isOk())
+                        .andExpect(MockMvcResultMatchers.jsonPath("$.length()").value(1));
+
+        responseString = resultActionsAfterPasswordChange.andReturn().getResponse().getContentAsString();
+        responseArray = mapper.readValue(responseString, User[].class);
+
+        Assertions.assertTrue(passwordEncoder.matches(TEST_UPDATED_PASSWORD, responseArray[0].getPassword()));
+    }
+
+    @Test
+    public void changePasswordWithInvalidTokenReturnsBadRequest() throws Exception {
+        User user = new User(TEST_USER_1);
+        String requestJson = convertObjectToStringForJson(user);
+
+        this.mockMvc
+                .perform(MockMvcRequestBuilders.post(USER_ENDPOINT).contentType(MediaType.APPLICATION_JSON).content(requestJson))
+                .andExpect(status().isOk());
+
+        ResultActions resultActionsUser =
+                this.mockMvc
+                        .perform(MockMvcRequestBuilders.get(USER_ENDPOINT).accept(MediaType.APPLICATION_JSON))
+                        .andExpect(status().isOk())
+                        .andExpect(MockMvcResultMatchers.jsonPath("$.length()").value(1));
+
+        String responseString = resultActionsUser.andReturn().getResponse().getContentAsString();
+        User[] responseArray = mapper.readValue(responseString, User[].class);
+
+        this.mockMvc
+                .perform(MockMvcRequestBuilders.post(USER_ENDPOINT + USER_VERIFY_EMAIL_PATH, TEST_USER_1.getEmail()))
+                .andExpect(status().isOk());
+
+        User userWithNewPassword = new User(TEST_USER_1);
+        userWithNewPassword.setPassword(TEST_UPDATED_PASSWORD);
+        String requestJson2 = convertObjectToStringForJson(userWithNewPassword);
+
+        List<PasswordResetToken> token = resetTokenRepository.findAllByUserId(responseArray[0].getId());
+        Assertions.assertEquals(1, token.size());
+
+        // try with random token
+        this.mockMvc
+                .perform(MockMvcRequestBuilders.post(USER_ENDPOINT + USER_CHANGE_PASSW_TOKEN_PATH, UUID.randomUUID())
+                        .contentType(MediaType.APPLICATION_JSON).content(requestJson2))
+                .andExpect(status().isBadRequest());
+
+        //token not consumed
+        token = resetTokenRepository.findAllByUserId(responseArray[0].getId());
+        Assertions.assertEquals(1, token.size());
+
+        ResultActions resultActionsAfterPasswordChange =
+                this.mockMvc
+                        .perform(MockMvcRequestBuilders.get(USER_ENDPOINT).accept(MediaType.APPLICATION_JSON))
+                        .andExpect(status().isOk())
+                        .andExpect(MockMvcResultMatchers.jsonPath("$.length()").value(1));
+
+        responseString = resultActionsAfterPasswordChange.andReturn().getResponse().getContentAsString();
+        responseArray = mapper.readValue(responseString, User[].class);
+
+        //password did not change
+        Assertions.assertFalse(passwordEncoder.matches(TEST_UPDATED_PASSWORD, responseArray[0].getPassword()));
     }
 }
